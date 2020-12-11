6204714c1cdc134ca9b703f3a1f8abf312343e5a
AKohut <a.kogut01@gmail.com>
2020-12-11 13:46:26 +0200

    all button are working

--- main.js
+++ main.js
@@ -8,12 +8,49 @@ let button_7 = document.getElementById("btn7");
 let button_8 = document.getElementById("btn8");
 let button_9 = document.getElementById("btn9");
 
-button_1.onclick = firstButtonFuncion();
-button_2.onclick = secondButtonFuncion();
-button_3.onclick = thirdButtonFuncion();
-button_4.onclick = fourthButtonFuncion();
-button_5.onclick = fifthButtonFuncion();
-button_6.onclick = sixthButtonFuncion();
-button_7.onclick = seventhButtonFuncion();
-button_8.onclick = eighthButtonFuncion();
-button_9.onclick = ninethButtonFuncion();
+button_1.addEventListener("click", firstButtonFuncion);
+button_2.addEventListener("click", secondButtonFuncion);
+button_3.addEventListener("click", thirdButtonFuncion);
+button_4.addEventListener("click", fourthButtonFuncion);
+button_5.addEventListener("click", fifthButtonFuncion);
+button_6.addEventListener("click", sixthButtonFuncion);
+button_7.addEventListener("click", seventhButtonFuncion);
+button_8.addEventListener("click", eighthButtonFuncion);
+button_9.addEventListener("click", ninethButtonFuncion);
+
+
+function firstButtonFuncion() {
+    console.log("1");
+}
+
+function secondButtonFuncion() {
+    console.log("2");
+}
+
+function thirdButtonFuncion() {
+    console.log("3");
+}
+
+function fourthButtonFuncion() {
+    console.log("4");
+}
+
+function fifthButtonFuncion() {
+    console.log("5");
+}
+
+function sixthButtonFuncion() {
+    console.log("6");
+}
+
+function seventhButtonFuncion() {
+    console.log("7");
+}
+
+function eighthButtonFuncion() {
+    console.log("8");
+}
+
+function ninethButtonFuncion() {
+    console.log("9");
+}
